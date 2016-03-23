281207ced1e9059c64b5dfb1b2acd0d9dd14bc38
Marco Ingrosso <marco.ingrosso@linksmt.it>
2016-03-23 20:58:01 +0100

    - bug: class db test

--- src/it/unisalento/pps/test/DatabaseTest.java
+++ src/it/unisalento/pps/test/DatabaseTest.java
@@ -6,6 +6,10 @@ public class DatabaseTest {
 
 	public static void main(String[] args) {
 		DbConnection dbConnection =  DbConnection.getInstance();
-		dbConnection.eseguiQuery("SELECT 1;");
+		try {
+			dbConnection.eseguiQuery("SELECT 1;");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
